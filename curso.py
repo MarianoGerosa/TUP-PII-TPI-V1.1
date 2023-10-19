15670273bf351c2114e48cf36485f5ad3664bfc8
Nicolas Robles <nicolasrobles98@gmail.com>
2023-10-19 07:34:43 -0300

    agrego curso, conecto profesor con usuario

---
+++ curso.py
@@ -0,0 +1,4 @@
+class curso():
+    def __init__(self, nombre:str, contraMatri:str) -> None:
+        self.__nombre = nombre
+        self.__contrasenia_matriculacion = contraMatri
--- profesor.py
+++ profesor.py
@@ -1,4 +1,7 @@
-class Profesor:
+from usuario import Usuario
+
+class Profesor(Usuario):
     def __init__(self, titulo:str, anioEgreso:int) -> None:
+        super().__init__()
         self.__titulo = titulo
         self._anio_egreso = anioEgreso
